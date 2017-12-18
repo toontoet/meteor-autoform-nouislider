faa024ac246cf481b2593df4c5916620f15f5272
Toon Toetenel <toon@breakingbits.nl>
2017-12-18 15:45:04 +0100

    Changed dependency to autoform 6.0.0

--- package.js
+++ package.js
@@ -15,7 +15,7 @@ Package.onUse(function(api) {
   api.use('ecmascript');
   api.use('templating@1.0.0');
   api.use('blaze@2.0.0');
-  api.use('aldeed:autoform@4.0.0 || 5.0.0');
+  api.use('aldeed:autoform@6.0.0');
   api.addFiles([
     'autoform-nouislider.html',
     'autoform-nouislider.js',
